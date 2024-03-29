095c384b10e00e94fc559d4b28b2ca0e9ec56d7d
nikneym <halildrk@gmail.com>
2024-03-29 17:09:17 +0300

    feat: integrate @tanstack/query

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import IndexPage from "./routes/page.tsx";
 import CityPage from "./routes/city/page.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./global.css";
 
+/* react router setup */
 const router = createBrowserRouter([
   /* index */
   {
@@ -17,15 +19,27 @@ const router = createBrowserRouter([
     path: "/city/:id",
     element: <CityPage />,
   },
-  /* 404 and other error cases */
+  /* 404 and all other routes */
   {
     path: "*",
     element: <>TODO</>,
   },
 ]);
 
+/* tanstack query setup */
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // requests become stale after 10 seconds
+      staleTime: 10_000,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
