025d1c54b7e4b0a2c1cc9fc3c84c6233be667ebb
nikneym <halildrk@gmail.com>
2024-03-29 17:10:36 +0300

    feat: implement search by location API

---
+++ src/api/search-location.ts
@@ -0,0 +1,58 @@
+import axiosInstance from "../utils/axios-instance";
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface Location {
+  id: number;
+  name: string;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  dt: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  sys: {
+    country: string;
+  };
+  /* FIXME: these two likely return some other types too, but we don't need them anyway for this project */
+  rain: null;
+  snow: null;
+  clouds: {
+    all: number;
+  };
+  weather: Weather[];
+}
+
+interface ResponseData {
+  message: string;
+  cod: number;
+  count: number;
+  list: Location[];
+}
+
+export default async function searchLocation(
+  location: string
+): Promise<ResponseData> {
+  const { data } = await axiosInstance({
+    method: "GET",
+    url: `find?q=${location}&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric`,
+  });
+
+  return data;
+}
