01c8b920e8c66f797071db9b7e605b574488886c
nikneym <halildrk@gmail.com>
2024-04-22 17:23:19 +0300

    fix: vite confiig

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      components: "/src/components",
-      hooks: "/src/hooks",
-      utils: "/src/utils",
-    },
-  },
   plugins: [react()],
-  base: "react-iweather",
 });
